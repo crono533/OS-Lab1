1575568a12efdf070f31d82637218e235d38dbf1
crono533 <disa.nanobot@mail.ru>
2023-12-16 14:55:43 +0700

    Исправлена ошибка

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,36 +1,45 @@
-#include <iostream>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <iostream>
 
 using namespace std;
-mutex mtx;  // Мьютекс для синхронизации
-condition_variable cv;  // Условная переменная для ожидания события
+
+mutex mtx;             // Мьютекс для синхронизации
+condition_variable cv; // Условная переменная для ожидания события
 bool provider_active = true;
 
 // Функция для потока-поставщика
-void provider_thread() {
-    while (true) {
-        this_thread::sleep_for(chrono::seconds(1));  // Задержка в 1 секунду
+void provider_thread()
+{
+    while (true)
+    {
+        this_thread::sleep_for(chrono::seconds(1)); // Задержка в 1 секунду
         unique_lock<mutex> lock(mtx);
         cout << "Поставщик: отправляю событие" << endl;
         provider_active = false;
-        cv.notify_one();  // Сообщаем потребителю, что событие отправлено
+        unique_lock<mutex> _Mtx_unlock (mtx); 
+        cv.notify_one(); // Сообщаем потребителю, что событие отправлено
     }
 }
 
 // Функция для потока-потребителя
-void consumer_thread() {
-    while (true) {
+void consumer_thread()
+{
+    while (true)
+    {
         unique_lock<mutex> lock(mtx);
-        cv.wait(lock, [] { return !provider_active; });  // Ожидаем, если активен поставщик
+        cv.wait(lock, []
+                { return !provider_active; }); // Ожидаем, если активен поставщик
         cout << "Потребитель: получил событие" << endl;
         provider_active = true;
-        cv.notify_one();  // Сообщаем поставщику, что событие обработано
+        unique_lock<mutex> _Mtx_unlock (mtx); 
+        cv.notify_one(); // Сообщаем поставщику, что событие обработано
     }
 }
 
-int main() {
+int main()
+{
     thread provider(provider_thread);
     thread consumer(consumer_thread);
     provider.join();
